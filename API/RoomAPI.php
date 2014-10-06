41f3e9bdb44663dec0f6c3e5e181a4c9aefeda4a
Andy Fitch <andy@parall.ax>
2014-10-06 17:04:07 +0100

    Set Topic

--- API/RoomAPI.php
+++ API/RoomAPI.php
@@ -144,11 +144,26 @@ class RoomAPI
      *
      * @param string $roomId   The id or name of the room
      * @param string $memberId The id, email address, or mention name (beginning with an '@') of the user
+     * @param string $message  The message displayed to a user when they are invited
      *
      * @return void
      */
     public function inviteMember($roomId, $memberId, $message)
     {
         $this->client->post("/v2/room/$roomId/invite/$memberId", ['message' => $message]);
+    }
+
+    /**
+     * Set a topic on a room
+     * More info: https://www.hipchat.com/docs/apiv2/method/set_topic
+     *
+     * @param string $roomId   The id or name of the room
+     * @param string $topic    The topic to be set
+     *
+     * @return void
+     */
+    public function setTopic($roomId, $topic)
+    {
+        $this->client->put("/v2/room/$roomId/topic", ['topic' => $topic]);
     }
 }
