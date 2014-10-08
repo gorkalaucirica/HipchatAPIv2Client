85ce6132335a6106c20bd4796a1d798963cdde63
Gorka Laucirica <gorka.lauzirika@gmail.com>
2014-10-08 09:36:57 +0200

    Changed constructor to allow custom browser

--- Client.php
+++ Client.php
@@ -3,6 +3,7 @@
 namespace GorkaLaucirica\HipchatAPIv2Client;
 
 use Buzz\Browser;
+use Buzz\Client\ClientInterface;
 use Buzz\Client\Curl;
 use GorkaLaucirica\HipchatAPIv2Client\Auth\AuthInterface;
 use GorkaLaucirica\HipchatAPIv2Client\Exception\RequestException;
@@ -14,33 +15,40 @@ class Client
     /** @var AuthInterface */
     protected $auth;
 
+    /** @var Browser */
+    protected $browser;
+
     /**
      * Client constructor
      *
-     * @param string        $baseUrl The base url to the api by default https://api.hipchat.com
-     * @param AuthInterface $auth    Authetication you want to use to access the api
+     * @param AuthInterface $auth Authentication you want to use to access the api
+     * @param Browser $browser Client you want to use, by default browser with curl will be used
      *
      * @return self
      */
-    public function __construct(AuthInterface $auth)
+    public function __construct(AuthInterface $auth, Browser $browser = null)
     {
         $this->auth = $auth;
+        if ($browser == null) {
+            $client = new Curl();
+            $this->browser = new Browser($client);
+        }
+        else {
+            $this->browser = $browser;
+        }
     }
 
     /**
      * Common get request for all API calls
      *
      * @param string $resource The path to the resource wanted. For example v2/room
-     * @param array  $query    Parameters to filter the response for example array('max-results' => 50)
+     * @param array $query Parameters to filter the response for example array('max-results' => 50)
      *
      * @return array Decoded array containing response
      * @throws Exception\RequestException
      */
     public function get($resource, $query = array())
     {
-        $curl = new Curl();
-        $browser = new Browser($curl);
-
         $url = $this->baseUrl . $resource;
         if (count($query) > 0) {
             $url .= "?";
@@ -52,10 +60,10 @@ class Client
 
         $headers = array("Authorization" => $this->auth->getCredential());
 
-        $response = $browser->get($url, $headers);
+        $response = $this->browser->get($url, $headers);
 
-        if ($browser->getLastResponse()->getStatusCode() > 299) {
-            throw new RequestException(json_decode($browser->getLastResponse()->getContent(), true));
+        if ($this->browser->getLastResponse()->getStatusCode() > 299) {
+            throw new RequestException(json_decode($this->browser->getLastResponse()->getContent(), true));
         }
 
         return json_decode($response->getContent(), true);
@@ -65,7 +73,7 @@ class Client
      * Common post request for all API calls
      *
      * @param string $resource The path to the resource wanted. For example v2/room
-     * @param array  $content  Parameters be posted for example:
+     * @param array $content Parameters be posted for example:
      *                              array(
      *                                'name'                => 'Example name',
      *                                'privacy'             => 'private',
@@ -79,9 +87,6 @@ class Client
      */
     public function post($resource, $content)
     {
-        $curl = new Curl();
-        $browser = new Browser($curl);
-
         $url = $this->baseUrl . $resource;
 
         $headers = array(
@@ -89,10 +94,10 @@ class Client
             'Authorization' => $this->auth->getCredential()
         );
 
-        $response = $browser->post($url, $headers, json_encode($content));
+        $response = $this->browser->post($url, $headers, json_encode($content));
 
-        if ($browser->getLastResponse()->getStatusCode() > 299) {
-            throw new RequestException(json_decode($browser->getLastResponse()->getContent(), true));
+        if ($this->browser->getLastResponse()->getStatusCode() > 299) {
+            throw new RequestException(json_decode($this->browser->getLastResponse()->getContent(), true));
         }
 
         return json_decode($response->getContent(), true);
@@ -102,7 +107,7 @@ class Client
      * Common put request for all API calls
      *
      * @param string $resource The path to the resource wanted. For example v2/room
-     * @param array  $content  Parameters be putted for example:
+     * @param array $content Parameters be putted for example:
      *                              array(
      *                                'name'                => 'Example name',
      *                                'privacy'             => 'private',
@@ -116,39 +121,41 @@ class Client
      */
     public function put($resource, $content = array())
     {
-        $curl = new Curl();
-        $browser = new Browser($curl);
-
         $url = $this->baseUrl . $resource;
         $headers = array(
             'Content-Type' => 'application/json',
             'Authorization' => $this->auth->getCredential()
         );
 
-        $response = $browser->put($url, $headers, json_encode($content));
+        $response = $this->browser->put($url, $headers, json_encode($content));
 
-        if ($browser->getLastResponse()->getStatusCode() > 299) {
-            throw new RequestException(json_decode($browser->getLastResponse()->getContent(), true));
+        if ($this->browser->getLastResponse()->getStatusCode() > 299) {
+            throw new RequestException(json_decode($this->browser->getLastResponse()->getContent(), true));
         }
 
         return json_decode($response->getContent(), true);
     }
 
+    /**
+     * Common delete request for all API calls
+     *
+     * @param string $resource The path to the resource wanted. For example v2/room
+     *
+     * @return array Decoded array containing response
+     * @throws Exception\RequestException
+     */
     public function delete($resource)
     {
-        $curl = new Curl();
-        $browser = new Browser($curl);
-
         $url = $this->baseUrl . $resource;
 
         $headers = array(
             'Authorization' => $this->auth->getCredential()
         );
 
-        $response = $browser->delete($url, $headers);
+        $response = $this->browser->delete($url, $headers);
 
-        if ($browser->getLastResponse()->getStatusCode() > 299) {
-            throw new RequestException(json_decode($browser->getLastResponse()->getContent(), true));
+        if ($this->browser->getLastResponse()->getStatusCode() > 299) {
+            throw new RequestException(json_decode($this->browser->getLastResponse()->getContent(), true));
         }
 
         return json_decode($response->getContent(), true);
